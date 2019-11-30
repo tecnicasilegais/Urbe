259649cf739f784526f429c9bebb4e97d597cfff
MarceloHeredia <marcelo.heredia@acad.pucrs.br>
2019-11-30 14:47:43 -0300

    mais testes Bairro

--- src/test/java/com/Urbe/testes/entidades/BairroTest.java
+++ src/test/java/com/Urbe/testes/entidades/BairroTest.java
@@ -1,16 +1,16 @@
 package com.Urbe.testes.entidades;
 
-import com.urbe.entidades.geometria.SituacaoReta;
 import com.urbe.entidades.Bairro;
-import com.urbe.entidades.geometria.Area;
 import com.urbe.entidades.geometria.Ponto;
-import com.urbe.entidades.geometria.Reta;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BairroTest
 {
@@ -25,12 +25,14 @@ public class BairroTest
     }
     
     @DisplayName("Testa Nome")
+    @Test
     public void testaNome(){
         Bairro bairro = Bairro.criaBairroRetangular("teste", ponto, 4, 6, 5.0);
         assertEquals("teste", bairro.getNome());
     }
 
     @DisplayName("Testa Custo")
+    @Test
     public void testaCusto(){
         assertEquals(5.0, bairroPadraoTestes.getCustoBase());
     }
@@ -44,15 +46,17 @@ public class BairroTest
 			"9.0",
 			"3.4",
 			"123123123.2"
-	})
+    })
 	public void testaMudaCusto(double novoCusto)
 	{
         bairroPadraoTestes.setCustoBase(novoCusto);
         assertEquals(novoCusto, bairroPadraoTestes.getCustoBase());
     }
 
+    @Test
     @DisplayName("Testa Excecao Custo Negativo")
     public void testaExcecao(){
-        
+        IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class, () -> bairroPadraoTestes.setCustoBase(-1), "Didnt throw");
+        assertTrue(thrown.getMessage().contains("Valor inválido"));    
     }
 }
