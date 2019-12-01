41c945576c8641cc2b6404d9c52f8ee8dac363d1
MarceloHeredia <marcelo.heredia@acad.pucrs.br>
2019-12-01 00:36:01 -0300

    javadoc, testes e mudança nos construtores pq o do java é muito feio

--- src/main/java/com/urbe/entidades/Cidade.java
+++ src/main/java/com/urbe/entidades/Cidade.java
@@ -2,7 +2,6 @@ package com.urbe.entidades;
 
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
