1c22876d7e798e32930ab1c5ecf2c4cfac11c678
ITWhiteRaccoon <eduardo.a@edu.pucrs.br>
2019-12-01 01:24:45 -0300

    tirei as gambi

--- src/main/java/com/urbe/interfaces/persistencia/RepositorioBairros.java
+++ src/main/java/com/urbe/interfaces/persistencia/RepositorioBairros.java
@@ -2,7 +2,6 @@ package com.urbe.interfaces.persistencia;
 
 import com.urbe.casos_de_uso.repositorios.IRepositorioBairros;
 import com.urbe.entidades.Bairro;
-import com.urbe.entidades.geometria.Ponto;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -17,15 +16,6 @@ public class RepositorioBairros implements IRepositorioBairros
 	{
 		bairros = new HashMap<>();
 
-		//FIXME implementar acesso à base Access
-		bairros.put("Petropolis", Bairro.novoBairroQuadrado("Petropolis", new Ponto(0, 12), 4, 10));
-		bairros.put("Ipiranga", Bairro.novoBairroRetangular("Ipiranga", new Ponto(4, 10), 4, 2, 6));
-		bairros.put("Solidao", Bairro.novoBairroRetangular("Solidao", new Ponto(0, 8), 2, 4, 5));
-		bairros.put("Bom Fim", Bairro.novoBairroRetangular("Bom Fim", new Ponto(2, 8), 6, 4, 10));
-		bairros.put("Vila Nova", Bairro.novoBairroRetangular("Vila Nova", new Ponto(8, 10), 5, 6, 15));
-		bairros.put("Alegria", Bairro.novoBairroRetangular("Alegria", new Ponto(0, 4), 5, 4, 10));
-		bairros.put("Boa Vista", Bairro.novoBairroRetangular("Boa Vista", new Ponto(5, 4), 5, 4, 15));
-		bairros.put("Gavea", Bairro.novoBairroRetangular("Gavea", new Ponto(10, 4), 2, 3, 20));
 	}
 
 	@Override
