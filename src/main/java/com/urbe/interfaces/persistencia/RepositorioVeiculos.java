5e56d4e15aa4c3ccae0b690f8e5743475a256087
ITWhiteRaccoon <eduardo.a@edu.pucrs.br>
2019-12-02 22:09:36 -0300

    spring wiring

--- src/main/java/com/urbe/interfaces/persistencia/RepositorioVeiculos.java
+++ src/main/java/com/urbe/interfaces/persistencia/RepositorioVeiculos.java
@@ -3,6 +3,7 @@ package com.urbe.interfaces.persistencia;
 import com.urbe.casos_de_uso.repositorios.IRepositorioVeiculos;
 import com.urbe.entidades.Retorno;
 import com.urbe.entidades.Veiculo;
+import com.urbe.entidades.VeiculosFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -15,9 +16,18 @@ public class RepositorioVeiculos implements IRepositorioVeiculos
 	private Map<String, Veiculo> veiculos;
 
 	@Autowired
-	public RepositorioVeiculos()
+	public RepositorioVeiculos(VeiculosFactory fabricaVeiculo)
 	{
 		veiculos = new HashMap<>();
+
+		Veiculo veiculo = fabricaVeiculo.createInstance("ABC1234", "GM", "Verde", "NORMAL");
+		veiculos.put(veiculo.placa(), veiculo);
+
+		veiculo = fabricaVeiculo.createInstance("XYZ5678", "WW", "Cinza", "SIMPLES");
+		veiculos.put(veiculo.placa(), veiculo);
+
+		veiculo = fabricaVeiculo.createInstance("AKK7336", "Honda", "Azul", "LUXO");
+		veiculos.put(veiculo.placa(), veiculo);
 	}
 
 	@Override
